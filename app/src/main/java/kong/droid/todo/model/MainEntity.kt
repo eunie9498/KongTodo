0b1f2600ce7f1a343f4423a71495140677ec4cf3
eunie9498 <bvegemilb@naver.com>
2022-05-29 23:14:58 +0900

    [Room] entity 1:n

---
+++ app/src/main/java/kong/droid/todo/model/MainEntity.kt
@@ -0,0 +1,27 @@
+package kong.droid.todo.model
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todos")
+class TodoEntity {
+    @PrimaryKey
+    var id: Int? = -1
+
+    var timeStamp: String? = ""
+}
+
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = TodoEntity::class,
+        parentColumns = arrayOf("timeStamp"), childColumns = arrayOf("time")
+    )]
+)
+
+class TodoItem {
+    var time: String? = ""
+    var name: String? = ""
+    var status: String? = ""
+    var importance: Int? = 0
+}
