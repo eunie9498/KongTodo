b89d5cf6192ee277e6877d56729bfdbf7d83eecd
eunie9498 <bvegemilb@naver.com>
2022-06-05 22:23:04 +0900

    [BaseActivity] exitApp

--- app/src/main/java/kong/droid/todo/BaseActivity.kt
+++ app/src/main/java/kong/droid/todo/BaseActivity.kt
@@ -1,12 +1,15 @@
 package kong.droid.todo
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.room.Room
 import kong.droid.todo.data.MainDB
+import kotlin.system.exitProcess
+
 
 abstract class BaseActivity<T: ViewDataBinding> (
     @LayoutRes val resID: Int): AppCompatActivity() {
@@ -26,9 +29,20 @@ abstract class BaseActivity<T: ViewDataBinding> (
 
     open fun initView() {
        // TODO("Not yet implemented")
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    fun showToast(msg: String){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+
+    open fun exitApp() {
+        finishAndRemoveTask()
+        exitProcess(0)
     }
 }
