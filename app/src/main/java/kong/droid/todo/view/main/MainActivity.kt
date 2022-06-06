c90e1ff697e33424bfd6f9d50368a3834d10bbfe
eunie9498 <bvegemilb@naver.com>
2022-06-06 15:28:17 +0900

    ViewPager2  - swipe Fragment

--- app/src/main/java/kong/droid/todo/view/main/MainActivity.kt
+++ app/src/main/java/kong/droid/todo/view/main/MainActivity.kt
@@ -3,17 +3,88 @@ package kong.droid.todo.view.main
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import kong.droid.todo.BaseActivity
 import kong.droid.todo.R
 import kong.droid.todo.data.TodoConstants
 import kong.droid.todo.databinding.ActivityMainBinding
+import kong.droid.todo.ui.TabItem
+import kong.droid.todo.viewModel.MainViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     var isBackPressed = false
+    lateinit var viewModel: MainViewModel
+    var PAGE_SIZE = 3
+    lateinit var tabItems: Array<TabItem>
 
-    override fun initView() {
-        val fragment = MainFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.fragContainer, fragment).commit()
+    override fun initView() = with(binding) {
+        viewModel = ViewModelProvider(this@MainActivity).get(MainViewModel::class.java)
+        viewModel = viewModel
+        lifecycleOwner = this@MainActivity
+
+        pagerAdapter = adapter
+        tabItems = arrayOf(binding.home, binding.list, binding.setting)
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                setSelectedPage(position)
+            }
+        })
+    }
+
+    fun setSelectedPage(position: Int) {
+        when (position % PAGE_SIZE) {
+            0 -> {
+                setSelectBg(binding.home)
+            }
+            1 -> {
+                //todo list
+                setSelectBg(binding.home)
+            }
+            2 -> {
+                //todo setting
+                setSelectBg(binding.list)
+            }
+            else -> {
+                setSelectBg(binding.setting)
+            }
+        }
+    }
+
+
+    val adapter = object : FragmentStateAdapter(supportFragmentManager, this.lifecycle) {
+        override fun createFragment(position: Int): Fragment {
+
+            return when (position % PAGE_SIZE) {
+                0 -> {
+                    MainFragment()
+                }
+                1 -> {
+                    //todo list
+                    MainFragment()
+                }
+                2 -> {
+                    //todo setting
+                    AddFragment()
+                }
+                else -> {
+                    AddFragment()
+                }
+            }
+        }
+
+        override fun getItemCount() = PAGE_SIZE
+    }
+
+
+    fun setSelectBg(v: TabItem) {
+        tabItems.find { it == v }!!.isSelected = true
+        tabItems.find { it != v }!!.isSelected = false
     }
 
 
@@ -29,7 +100,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             }
         }
     }
-
 
     override fun onBackPressed() {
         if (!isBackPressed) {
